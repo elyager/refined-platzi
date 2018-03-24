74d215bc4c2161044e1263b7e47195c6508237de
Elyager <elyager@gmail.com>
2018-03-24 11:26:28 -0700

    Remove unnecessary cast

--- inject.js
+++ inject.js
@@ -9,22 +9,22 @@
   }
 
   let $contentDuration = document.querySelectorAll('.MaterialContent-duration')
-  let totalMinutes = Number(0)
+  let totalMinutes = 0
 
   $contentDuration.forEach( (item) => {
-    let minutes = parseInt(item.innerText, 10) || 0    
-    totalMinutes += minutes      
+    let minutes = parseInt(item.innerText, 10) || 0
+    totalMinutes += minutes
   })
 
-  let totalHours = Math.floor(totalMinutes / 60)    
+  let totalHours = Math.floor(totalMinutes / 60)
   let remainingMinutes = totalMinutes % 60
   let message = `Duración Total: ${totalHours} horas con ${remainingMinutes} minutos`
-  
+
   let spanElement = document.createElement('span')
   spanElement.style.color = '#2295cf'
   spanElement.id = 'course-duration'
   spanElement.textContent = message
-  
+
   $topBanner.appendChild(spanElement)
 
 })()
